cec70549a6d6994c4b68792c45a0a0e02a601d03
Kasturi <75318338+KasturiS899@users.noreply.github.com>
2023-07-12 11:21:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
+	let binaryNum = new Array(32);
+	let i = 0;
+    while (num > 0) {
+        binaryNum[i] = n % 2;
+        n = Math.floor(n / 2);
+        i++;
+    }
+ 
+    // printing binary array in reverse order
+    for (let j = i - 1; j >= 0; j--)
+        document.write(binaryNum[j]);
   
 }
 
